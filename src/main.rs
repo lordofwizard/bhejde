070aca944d21e518d6fa86a33d2b039ddeb6aea4
lordofwizard <advaitpandharpurkar619@gmail.com>
2023-10-11 20:12:33 +0530

    overwrought-pot

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::process::{Command, exit};
 use names::Generator;
+use colored::*;
 
-fn update_commit_push() {
+fn update_commit_push(commit_message: Option<&str>) {
     // Command 1: Add all files recursively to git repo
     let add_command = Command::new("git")
         .arg("add")
@@ -10,21 +11,26 @@ fn update_commit_push() {
         .expect("Failed to execute git add command\nNo changes probably");
 
     if !add_command.status.success() {
-        eprintln!("Error: Failed to add files to the git repo.\nChanges hain na? check kar.");
+        eprintln!("{}", "Error: Failed to add files to the git repo.\nChanges hain na? check kar.".red());
         exit(1);
     }
 
     // Command 2: Commit all changes
+    let commit_message = match commit_message {
+        Some(msg) => msg.to_string(),
+        None => name_generator(),
+    };
+
     let commit_command = Command::new("git")
         .arg("commit")
         .arg("-a")
         .arg("-m")
-        .arg(name_generator())
+        .arg(&commit_message)
         .output()
         .expect("Failed to execute git commit command");
 
     if !commit_command.status.success() {
-        eprintln!("Error: Failed to commit changes.");
+        eprintln!("{}", "Error: Failed to commit changes.".red());
         exit(1);
     }
 
@@ -37,16 +43,25 @@ fn update_commit_push() {
         .expect("Failed to execute git push command");
 
     if !push_command.status.success() {
-        eprintln!("Error: Failed to push changes to remote.\nInternet Check Kar\nRepo remote check kar nahi toh");
+        eprintln!("{}", "Error: Failed to push changes to remote.\nInternet Check Kar\nRepo remote check kar nahi toh".red());
         exit(1);
     }
 
-    println!("Successfully added, committed, and pushed changes!\nKAAY VISHAY NAI NANA, pathavle warti");
+    println!("{}", "Successfully added, committed, and pushed changes!\nKAAY VISHAY NAI NANA, pathavle warti".green());
 }
-fn name_generator() -> String{
-let mut generator = Generator::default();
+
+fn name_generator() -> String {
+    let mut generator = Generator::default();
     generator.next().unwrap()
 }
+
 fn main() {
-    update_commit_push();
+    let args: Vec<String> = std::env::args().collect();
+    let commit_message = if args.len() > 1 {
+        Some(&args[1])
+    } else {
+        None
+    };
+    update_commit_push(commit_message.map(|x| x.as_str()));
+
 }
